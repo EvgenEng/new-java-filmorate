e6ede8ee8644c51746f725a519ada23ebd77a973
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-05-08 20:12:21 +0300

    Fix Checkstyle: remove unused import in FilmDbStorage

--- src/main/java/ru/yandex/practicum/storage/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/storage/FilmDbStorage.java
@@ -17,7 +17,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 @Component
