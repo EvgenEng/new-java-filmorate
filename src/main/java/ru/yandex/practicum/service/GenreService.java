2a00c49fa9dd6e30129a9b9c177b844b2425d26f
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-05-11 01:29:17 +0300

    Fix error Checkstyle in GenreService

--- src/main/java/ru/yandex/practicum/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/service/GenreService.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.service;
 import ru.yandex.practicum.model.GenreDto;
 
 public interface GenreService {
+
     GenreDto getGenreDto(Integer genreId);
+    
     boolean existsById(Integer id);
 }
