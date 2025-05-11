44488fd9feb18e25089687bf54a64f72a1b24c82
EvgenEng <zhenya-chaplygin@yandex.ru>
2025-05-11 13:17:23 +0300

    Fix POSTMAN

--- src/main/java/ru/yandex/practicum/FilmorateApplication.java
+++ src/main/java/ru/yandex/practicum/FilmorateApplication.java
@@ -2,10 +2,8 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
-@EnableWebMvc
 public class FilmorateApplication {
     static {
         try {
